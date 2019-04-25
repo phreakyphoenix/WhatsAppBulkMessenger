1e53e3864d176dc669773fe59c058359ea44d1f9
phreakyphoenix <adityajyotipaul007@gmail.com>
2019-04-25 23:31:59 +0530

    Major speed imporvements remove stupid popup etc

--- run.py
+++ run.py
@@ -1,27 +1,37 @@
-# %%from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import UnexpectedAlertPresentException
-
 import urllib
 from time import sleep
-# str(raw_input("Enter the message:\n"))
+
+# %%from pyvirtualdisplay import Display
 # display = Display(visible=0, size=(800, 600))
 # display.start()
+
 driver = webdriver.Chrome()
 
-phone = ['9514786321','123456789','987654321']                  #enter comma separated strings with 10 digit phone numbers or read them from a file
-msg = "Thank you for your response. Welcome aboard!"            #message to be sent to everyone, you can also read it as a dict from a file with ph nos as keys
+phone = []                                                      #enter comma separated 10 digit phone numbers here or read them from the numbers_file
+with open ('numbers.txt') as numbers_file:
+    for line in numbers_file:
+        phone.append(str(line))
+
+msg = "3Thank you for your response. Welcome aboard!"            #message to be sent to everyone, you can also read it as a dict from a file with ph nos as keys
 
 msg = urllib.quote(msg)                                         #url-encode the message, use other functios for handling dictionaries, not recommended
 driver.get('https://web.whatsapp.com')                          #first call without delay in order to scan qr code
+css_selector = '#main > footer > div._3pkkz.copyable-area > div._1Plpp > div > div._2S1VP.copyable-text.selectable-text'
+
 for i in range(len(phone)):
     url='https://web.whatsapp.com/send?phone=91' + phone[i] + "&text=" + msg
     print url
     driver.get(url)
-    sleep(5)
-    try:
-        driver.find_element_by_css_selector('#main > footer > div._3pkkz.copyable-area > div._1Plpp > div > div._2S1VP.copyable-text.selectable-text').send_keys(Keys.RETURN)
-        sleep(5)
-    except UnexpectedAlertPresentException:                     #handles stupid popup asking if you really want to leave the page
-        pass
+    sleep(4)
+    for i in range(100):
+        try:
+            driver.find_element_by_css_selector(css_selector).send_keys(Keys.RETURN)
+            driver.execute_script("window.onbeforeunload = function() {};")
+            break
+        except:
+            print("not yet")
+            sleep(1)
